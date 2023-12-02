0f1a561e615e6c2cd9dfa30466032991ecaf9bdb
Reed Mullanix <reedmullanix@gmail.com>
2023-12-01 20:40:29 -0500

    wip: printing is broken

--- day-1.asm
+++ day-1.asm
@@ -8,7 +8,7 @@ section .text
 
 %macro begin_print 0
 	push ax
-        mov ax, 0xb800          ; Console memory is at 0xb8000; set up a segment
+	mov ax, 0xb800          ; Console memory is at 0xb8000; set up a segment
         mov es, ax              ; for the start of the console text.
 	xor di, di              ; Clear the data index so that 'stosb' pushes to video memory.
 	pop ax
@@ -31,13 +31,20 @@ main:
 	mov dl, 79              ; 79 cols of chars
 	int 0x10                ; Ask the BIOS to scroll the screen
 
-                                ; Read data from disk
-        call load_file		;
+        call load_file		; Read all the data from dist
         call part1
-
+	push bx
+	push cx
 	begin_print
+	mov ax, 1
         call print_digits
-	mov bx, cx
+	mov ax, 2
+        call print_digits
+	mov ax, 3
+        call print_digits
+	mov ax, 4
+        call print_digits
+	mov ax, 
         call print_digits
 sleep:
         hlt                      ; Halts CPU until the next external interrupt is fired
@@ -58,7 +65,6 @@ load_file:
         mov es, bx              ; so let's set the sector to 0x0,
         mov bx, disk_buffer     ; Write to 0x7e00
         int 0x13                ; Ask the BIOS to read the data
-
         
         mov si, 0x02                ; Set up the source index for 'lodsb'
         mov bx, (disk_num / 0x10)   ; and set the data segment to '0x7e00'
@@ -118,38 +124,93 @@ part1:
 	push ax                 ; Finally, push the low portion.
 	jmp .read_line
 .done:
-	pop bx
 	pop cx
+	pop bx
         ret
 
-;; Calling Convention:
-;; Input:
-;; %bx holds number to be printed
-;; %cx holds number of digits, 1 indexed.
+hex_table:
+	db '0123456789abcdef'
+
+
+;; Prints contents of %ax as hex.
+;; Clobbers %bx
 print_digits:
-        mov ax, bx              ; Move the number into %ax,
-        xor cx, cx              ; We will use %cx to count the number of digits
-.push:
-        cmp ax, 0
-        je .print
-        mov dx, 0               ; Clear %dx so that we don't have an offset for the division 
-        mov bx, 10              ; Set %cx to the constant 10 for the DIV
-        div bx                  ; Divide %ax by 10, storing renaminder in %dx
-        add dx, `0`             ; Convert %dx to ASCII
-        push dx                 ; Push %dl to the stack, so we can pop off in reverse
-        inc cx                  ; Increment the count of digits
-        jmp .push
-.print:
-        cmp cx, 0
-        je .done
-        pop ax
-        stosb
-        mov ax, 0x07
-        stosb
-        dec cx
-        jmp .print
-.done:
-        ret
+	push ax			; Save contents of %ax, push '0x' to video memory.
+	mov ax, `0`
+	stosb			
+	mov al, 0x07		
+	stosb			
+	mov ax, `x`
+	stosb			
+	mov al, 0x07		
+	stosb
+
+	pop ax
+	push ax
+	mov bx, hex_table
+
+	mov al, ah 		; We will print one byte at a time, starting with the high byte
+	shr al, 4 		; %al now has the low nibble.
+	and ah, 0x0f 		; %ah now has the high nibble.
+	xlat 			; %al now contains the right character for the high nibble.
+	stosb			; Push that byte to video memory
+	mov al, 0x07		; Add color information
+	stosb			; Push color information
+	xchg al, ah		; Do the same for the low nibble
+	xlat 			; %al now contains the right character for the low nibble.
+	stosb			; Push that byte to video memory
+	mov al, 0x07		; Add color information
+	stosb			; Push color information
+
+	pop ax
+	mov ah, al 		; On to the low byte
+	shr al, 4 		; %al now has the low nibble.
+	and ah, 0x0f 		; %ah now has the high nibble.
+	xlat 			; %al now contains the right character for the high nibble.
+	stosb			; Push that byte to video memory
+	mov al, 0x07		; Add color information
+	stosb			; Push color information
+	xchg al, ah		; Do the same for the low nibble
+	xlat 			; %al now contains the right character for the low nibble.
+	stosb			; Push that byte to video memory
+	mov al, 0x07		; Add color information
+	stosb			; Push color information
+
+	mov ax, ` `  		; print a space character
+	stosb			
+	mov al, 0x07		
+	stosb
+
+	ret
+
+
+; ;; Input:
+; ;; %bx holds number to be printed
+; ;; %cx holds number of digits, 1 indexed.
+; print_digits:
+;         mov ax, bx              ; Move the number into %ax,
+;         xor cx, cx              ; We will use %cx to count the number of digits
+; .push:
+;         cmp ax, 0
+;         je .print
+;         mov dx, 0               ; Clear %dx so that we don't have an offset for the division 
+;         mov bx, 10              ; Set %cx to the constant 10 for the DIV
+;         div bx                  ; Divide %ax by 10, storing renaminder in %dx
+;         add dx, `0`             ; Convert %dx to ASCII
+;         push dx                 ; Push %dl to the stack, so we can pop off in reverse
+;         inc cx                  ; Increment the count of digits
+;         jmp .push
+; .print:
+;         cmp cx, 0
+;         je .done
+;         pop ax
+;         stosb
+;         mov ax, 0x07
+;         stosb
+;         dec cx
+;         jmp .print
+; .done:
+;         ret
 
 times 510-($-$$) db 0     ; Pad to 510 bytes
 dw 0xaa55                 ; Add boot magic word to mark us as bootable
