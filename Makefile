18aab1a6acf798add6e1b1a04466ee610e12467c
Reed Mullanix <reedmullanix@gmail.com>
2023-12-04 15:34:43 -0500

    wip: minor optimizations

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@
 day-1.img: day-1.asm day-1-input.txt
 	nasm day-1.asm -f bin -o day-1.img
 
-day-1: day-1.img
-	rlwrap bochs -qf /dev/null 'ata0-master: type=disk, path="day-1.img", mode=flat, cylinders=1, heads=1, spt=63' 'boot: disk' 'display_library: sdl2' 'megs: 128'
+day-1: day-1.img day-1.cfg
+	bochs -qf day-1.cfg -rc bochs-input.txt
--- day-1.asm
+++ day-1.asm
@@ -2,8 +2,9 @@
 [org 0x7c00]     ; BIOS will load us to this address
 section .text
 
-%define disk_num (0x7c00 + 512)
-%define disk_buffer (disk_num + 0x02) ; Retain the disk number, don't want unaligned reads
+%define disk_num (0x7c00 + 0x200)
+%define disk_buffer disk_num ; Retain the disk number, don't want unaligned reads
+%define data_start (0x7c00 + 0x600)
 %define num_sectors 63
 
 %macro begin_print 0
@@ -32,11 +33,13 @@ main:
 	int 0x10                ; Ask the BIOS to scroll the screen
 
         call load_file		; Read all the data from dist
+
         call part1
+
 	begin_print
-	pop ax
+	mov ax, bx
         call print_digits
-	pop ax
+	mov ax, cx
         call print_digits
 sleep:
         hlt                      ; Halts CPU until the next external interrupt is fired
@@ -47,21 +50,24 @@ sleep:
 
 ;;; Load the input file into memory, starting at 0x7e00
 load_file:
-        mov ah, 0x02            ; We want to read sectors, so we set %ah to 0x02
-        mov al, num_sectors     ; Number of sectors to read
-        mov ch, 0               ; Cylinder index
-        mov cl, 2               ; Sector index
-        mov dh, 0               ; Head index
-        mov dl, [disk_num]      ; Disk number
-        xor bx, bx              ; INT 0x13 will write to %es:%bx,
-        mov es, bx              ; so let's set the sector to 0x0,
-        mov bx, disk_buffer     ; Write to 0x7e00
-        int 0x13                ; Ask the BIOS to read the data
+        mov ah, 0x02                       ; We want to read sectors, so we set %ah to 0x02
+        mov al, num_sectors                ; Number of sectors to read
+        mov ch, 0                          ; Cylinder index
+        mov cl, 2                          ; Sector index
+        mov dh, 0                          ; Head index
+        mov dl, [disk_num]                 ; Disk number
+        xor bx, bx                         ; INT 0x13 will write to %es:%bx,
+        mov es, bx                         ; so let's set the sector to 0x0,
+        mov bx, disk_buffer                ; Write to 0x7e00
+        int 0x13                           ; Ask the BIOS to read the data
         
-        mov si, 0x02                ; Set up the source index for 'lodsb'
-        mov bx, (disk_num / 0x10)   ; and set the data segment to '0x7e00'
+        mov si, 0x00                       ; Set up the source index for 'lodsb'
+        mov bx, (data_start / 0x10)          ; and set the data segment to the start of the data.
         mov ds, bx
         ret
+
+times 510-($-$$) db 0     ; Pad to 510 bytes
+dw 0xaa55                 ; Add boot magic word to mark us as bootable
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Solution
@@ -72,6 +78,7 @@ load_file:
 ;; Clobbers %ax, %bx, %cx, %dx
 ;; Updates %si to the end of the string.
 ;; Outputs low portion in %cx, high in %bx
+
 part1:
 	push 0
 	push 0
@@ -79,6 +86,7 @@ part1:
 	xor ax, ax              ; Initialize %ax to 0
         xor bx, bx              ; Initialize %bx to 0 
         xor cx, cx              ; Initialize %cx to 0
+	xor dx, dx              ; Holds first digit seen
 .read_char:
         lodsb                   ; Load a byte into %al, incrementing %si
 	cmp al, 0               ; If we are looking at 0, then we have hit the end of the input.
@@ -88,17 +96,12 @@ part1:
         cmp al, `a`             ; Check if we are looking at a letter,
         jge .read_char          ; If we are, then continue the loop.
         sub ax, `0`             ; Convert from ASCII to the actual value
-        cmp bl, 0               ; Check to see if this is the first digit we've seen
-        jne .not_first          ; If it isn't skip ahead to next part of state machine
-.first:
-        mov bl, al              ; Store the first digit we've seen in %bx
-	mov cl, al  		; Also store it in %cx, as we ought to count it twice if it's the only digit
-        jmp .read_char
-.not_first:
-        mov cl, al              ; Store the digit in %cx, clobbering any previous value
+        cmp dl, 0               ; Check to see if this is the first digit we've seen
+	cmove dx, ax            ; Only store the first digit we've seen
+	mov cx, ax              ; Always update the second digit
         jmp .read_char
 .add_digits:
-        mov al, bl              ; Move the first digit into %ax,
+        mov al, dl              ; Move the first digit into %ax,
         mov dx, 10              ; multiply %ax by 10
         mul dx                  
         add al, cl              ; Next, add on the second digit.
@@ -170,239 +173,153 @@ part1:
 ;; *, e -> 5
 ;; *, n -> 6
 
-jump_table:
-	dw 0
-	dw (part2.s01 - part2.s00)
-	dw (part2.s01 - part2.s00)
-	dw (part2.s02 - part2.s00)
-	dw (part2.s03 - part2.s00)
-	dw (part2.s04 - part2.s00)
-	dw (part2.s05 - part2.s00)
-	dw (part2.s06 - part2.s00)
-	dw (part2.s07 - part2.s00)
-	dw (part2.s08 - part2.s00)
-	dw (part2.s09 - part2.s00)
-	dw (part2.s10 - part2.s00)
-	dw (part2.s11 - part2.s00)
-	dw (part2.s12 - part2.s00)
-	dw (part2.s13 - part2.s00)
-	dw (part2.s14 - part2.s00)
-	dw (part2.s15 - part2.s00)
-	dw (part2.s16 - part2.s00)
-	dw (part2.s17 - part2.s00)
-	dw (part2.s18 - part2.s00)
-	dw (part2.s19 - part2.s00)
-	dw (part2.s20 - part2.s00)
-	dw (part2.s21 - part2.s00)
-	dw (part2.s22 - part2.s00)
-	dw (part2.s23 - part2.s00)
-	dw (part2.s24 - part2.s00)
+; jump_table:
+; 	dw 0
+; 	dw (part2.s01 - part2.s00)
+; 	dw (part2.s01 - part2.s00)
+; 	dw (part2.s02 - part2.s00)
+; 	dw (part2.s03 - part2.s00)
+; 	dw (part2.s04 - part2.s00)
+; 	dw (part2.s05 - part2.s00)
+; 	dw (part2.s06 - part2.s00)
+; 	dw (part2.s07 - part2.s00)
+; 	dw (part2.s08 - part2.s00)
+; 	dw (part2.s09 - part2.s00)
+; 	dw (part2.s10 - part2.s00)
+; 	dw (part2.s11 - part2.s00)
+; 	dw (part2.s12 - part2.s00)
+; 	dw (part2.s13 - part2.s00)
+; 	dw (part2.s14 - part2.s00)
+; 	dw (part2.s15 - part2.s00)
+; 	dw (part2.s16 - part2.s00)
+; 	dw (part2.s17 - part2.s00)
+; 	dw (part2.s18 - part2.s00)
+; 	dw (part2.s19 - part2.s00)
+; 	dw (part2.s20 - part2.s00)
+; 	dw (part2.s21 - part2.s00)
+; 	dw (part2.s22 - part2.s00)
+; 	dw (part2.s23 - part2.s00)
+; 	dw (part2.s24 - part2.s00)
 
-state_0_table:
-	db '00005300000006100042000000'
+; state_0_table:
+; 	db '00005300000006100042000000'
+
+
+; %macro transition 2
+; 	cmp ax, $1
+; 	mov di, $2
+; 	cmove dx, di
+; 	je .read_char
+; 	jne .s00
+; %endmacro
+
+; %macro transition_accept 3
+; 	cmp al, $1
+; 	mov di, $2
+; 	cmove dx, di
+; 	mov di, $3
+; 	cmove ax, di
+; 	je .set_digit
+; 	jne .s00
+; %endmacro
+
+; %macro transition_branch 4
+; 	cmp al, $1
+; 	mov di, $2
+; 	cmove dx, di
+; 	je .read_char
+; 	cmp ax, $3
+; 	mov di, $4
+; 	cmove dx, di
+; 	je .read_char
+; 	jne .s00
+; %endmacro
+
+; %macro transition_branch_accept 5
+; 	cmp al, $1
+; 	mov di, $2
+; 	cmove dx, di
+; 	mov di, $3
+; 	cmove ax, di
+; 	je .set_digit
+; 	cmp al, $4
+; 	mov di, $5
+; 	cmove dx, di
+; 	je .read_char
+; 	jne .s00
+; %endmacro
+
+; part2:
+; 	push 0
+; 	push 0
+; .read_line:
+; 	xor ax, ax              ; Initialize %ax to 0
+;         xor bx, bx              ; Initialize %bx to 0 
+;         xor cx, cx              ; Initialize %cx to 0
+;         xor dx, dx              ; Initialize %dx to 0
+; 	;; General Algorithm
+; .read_char:
+;         lodsb                   ; Load a byte into %al, incrementing %si
+; 	cmp al, 0               ; If we are looking at 0, then we have hit the end of the input.
+; 	je .done                ; This means we are done, so we bail out.
+;         cmp al, `\n`            ; Check if we are looking at a newline,
+;         je .add_digits          ; if we are, then we proceed to add the 2 digits we've seen.
+;         cmp al, `a`             ; Check if we are looking at a letter,
+;         jle .digit              ; If we are, then enter the state machine. If not, then we are looking at a digit.
+; 	push bx
+; 	mov bx, jump_table 	
+; 	xchg al, dl 		; Swap %al and %dl so we can do a table lookup
+; 	xlat 			; TODO: These should be byte offsets?
+; 	xchg al, dl
+; .s00: 			
+; 	push bx 		; Grab the new state from the table
+; 	mov bx, state_0_table
+; 	xlat
+; 	mov dl, al
+; 	pop bx
+; 	je .read_char
+; .s01: transition               `n`, 7
+; .s02: transition_branch        `w`, 8, `h`, 9
+; .s03: transition_branch        `o`, 12, `i`, 4
+; .s04: transition_branch        `i`, 16, `e`, 17
+; .s05: transition               `i`, 20
+; .s06: transition               `i`, 23
+; .s07: transition_branch_accept `e`, 5, 1, `i`, 23
+; .s08: transition_accept        `o`, 1, 2
+; .s09: transition               `r`, 10
+; .s10: transition               `e`, 11
+; .s11: transition_branch_accept `e`, 5, 3, `i`, 21
+; .s12: transition_branch        `u`, 13, `n`, 7
+; .s13: transition_accept        `r`, 0, 4
+; .s14: transition               `v`, 15
+; .s15: transition_accept        `e`, 5, 5
+; .s16: transition_accept        `x`, 0, 6
+; .s17: transition_branch        `v`, 18, `g`, 21
+; .s18: transition               `e`, 19
+; .s19: transition_branch_accept `n`, 6, 7, `g`, 21
+; .s20: transition               `g`, 21
+; .s21: transition               `h`, 22
+; .s22: transition_accept        `t`, 2, 8
+; .s23: transition               `n`, 24
+; .s24: transition_branch_accept `e`, 5, 9, `i`, 23
+	
+
+; .digit:
+; 	sub al, `0` 		; Convert to ASCII
+; .set_digit:
+;         cmp bl, 0               ; Check to see if this is the first digit we've seen
+; 	cmove bx, ax 		; If it is, move it into both positions
+; 	cmove cx, ax
+; 	cmovne cx, ax		; If it isn't, only update the second digit
+	; 	jmp .read_char
 
 hex_table:
 	db '0123456789abcdef'
 
-part2:
-	push 0
-	push 0
-.read_line:
-	xor ax, ax              ; Initialize %ax to 0
-        xor bx, bx              ; Initialize %bx to 0 
-        xor cx, cx              ; Initialize %cx to 0
-        xor dx, dx              ; Initialize %dx to 0
-	;; General Algorithm
-.read_char:
-        lodsb                   ; Load a byte into %al, incrementing %si
-	cmp al, 0               ; If we are looking at 0, then we have hit the end of the input.
-	je .done                ; This means we are done, so we bail out.
-        cmp al, `\n`            ; Check if we are looking at a newline,
-        je .add_digits          ; if we are, then we proceed to add the 2 digits we've seen.
-        cmp al, `a`             ; Check if we are looking at a letter,
-        jle .digit              ; If we are, then enter the state machine. If not, then we are looking at a digit.
-	push bx
-	mov bx, jump_table 	
-	xchg al, dl 		; Swap %al and %dl so we can do a table lookup
-	xlat 			; TODO: These should be byte offsets?
-	xchg al, dl
-.s00: 			
-	push bx 		; Grab the new state from the table
-	mov bx, state_0_table
-	xlat
-	mov dl, al
-	pop bx
-	je .read_char
-.s01:
-	cmp al, `n`
-	cmove dl, 7
-	je .read_char
-	jne .s00
-.s02:
-	cmp al, `w`
-	cmove dl, 8
-	je .read_char
-	cmp al, `h`
-	cmove dl, 9
-	je .read_char
-	jne .s00
-.s03:
-	cmp al, `o`
-	cmove dl, 12
-	je .read_char
-	cmp al, `i`
-	cmove dl, 4
-	je .read_char
-	jne .s00
-.s04:
-	cmp al, `i`
-	cmove dl, 16
-	je .read_char
-	cmp al, `e`
-	cmove dl, 17
-	je .read_char
-	jne .s00
-.s05:
-	cmp al, `i`
-	cmove dl, 20
-	je .read_char
-	jne .s00
-.s06:
-	cmp al, `i`
-	cmove dl, 21
-	je .read_char
-	jne .s00
-.s07:
-	cmp al, `e`
-	cmove dl, 5
-	cmove al, 1
-	je .set_digit
-	cmp al, `i`
-	cmove dl, 23
-	je .read_char
-	jne .s00
-.s08:
-	cmp al, `o`
-	cmove dl, 1
-	cmove al, 2
-	je .set_digit
-	jne .s00
-.s09:
-	cmp al, `r`
-	cmove dl, 11
-	je .read_char
-	jne .s00
-.s10:
-	cmp al, `e`
-	cmove dl, 11
-	je .read_char
-	jne .s00
-.s11:
-	cmp al, `e`
-	cmove dl, 5
-	cmove al, 3
-	je .set_digit
-	cmp al, `i`
-	cmove dl, 21
-	je .read_char
-	jne .s00
-.s12:
-	cmp al, `u`
-	cmove dl, 13
-	je .read_char
-	cmp al, `n`
-	cmove dl, 7
-	je .read_char
-	jne .s00
-.s13:
-	cmp al, `r`
-	cmove al, 4
-	je .set_digit
-	jne .s00
-.s14:
-	cmp al, `v`
-	cmove dl, 15
-	je .read_char
-	jne .s00
-.s15:
-	cmp al, `e`
-	cmove al, 5
-	cmove dl, 5
-	je .set_digit
-	jne .s00
-.s16:
-	cmp al, `x`
-	cmove al, 6
-	cmove dl, 0
-	je .set_digit
-	jne .s00
-.s17:
-	cmp al, `v`
-	cmove dl, 18
-	je .read_char
-	cmp al, `g`
-	cmove dl, 21
-	je .read_char
-	jne .s00
-.s18:
-	cmp al, `e`
-	cmove dl, 19
-	je .read_char
-	jne .s00
-.s19:
-	cmp al, `n`
-	cmove al, 7
-	cmove dl, 6
-	je .set_digit
-	cmp al, `g`
-	cmove dl, 21
-	je .read_char
-	jne .s00
-.s20:
-	cmp al, `g`
-	cmove dl, 21
-	je .read_char
-	jne .s00
-.s21:
-	cmp al, `h`
-	cmove dl, 22
-	je .read_char
-	jne .s00
-.s22:
-	cmp al, `t`
-	cmove al, 8
-	cmove dl, 2
-	je .set_digit
-	jne .s00
-.s23:
-	cmp al, `n`
-	cmove dl, 24
-	je .read_char
-	jne .s00
-.s24:
-	cmp al, `e`
-	cmove al, 9
-	cmove dl, 5
-	je .set_digit
-	cmp al, `i`
-	cmove dl, 23
-	je .read_char
-	jne .s00
-
-	
-
-.digit:
-	sub al, `0` 		; Convert to ASCII
-.set_digit:
-        cmp bl, 0               ; Check to see if this is the first digit we've seen
-	cmove bl, al 		; If it is, move it into both positions
-	cmove cl, al
-	cmovne cl, al		; If it isn't, only update the second digit
-	jmp .read_char
-
 ;; Prints contents of %ax as hex.
 ;; Clobbers %bx
 print_digits:
+	push bx
 	push ax			; Save contents of %ax, push '0x' to video memory.
 	mov ax, `0`
 	stosb			
@@ -449,10 +366,9 @@ print_digits:
 	mov al, 0x07		
 	stosb
 
+	pop bx
 	ret
-
-times 510-($-$$) db 0     ; Pad to 510 bytes
-dw 0xaa55                 ; Add boot magic word to mark us as bootable
+times (3 * 512)-($-$$) db 0    ; Pad out to a multiple of 512 bytes
 
 incbin "day-1-input.txt"
 
